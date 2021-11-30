47e3b31740ae06aab7613ef5571719aad9c44ebe
TomiG06 <thomasgkountas@gmail.com>
2021-11-30 14:52:37 +0200

    corrected sqrt to pow

--- AlgorithmsNMore/sieveOfEratosthenes.py
+++ AlgorithmsNMore/sieveOfEratosthenes.py
@@ -2,16 +2,14 @@
 #Check more about it here: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 #Briefly: Algorithm to find all prime numbers of a given range
 
-import math
-
 limit = int(input("Enter limit: "))
 ret = [n for n in range(2, limit+1)]
 
 for n in ret:
-    if math.sqrt(n) < limit:
+    if n**2 < limit:
         for num in ret:
             if num%n==0 and n!=num:
                 ret.remove(num)
     else:
+        print(ret)
         break
-print(ret)
