cd1c351bce78589bf0e0be26aab6c42aa0de6934
TomiG06 <thomasgkountas@gmail.com>
2021-12-02 16:05:14 +0200

    removed Stack.TOP and used -1 instead

--- blackJack.py
+++ blackJack.py
@@ -4,12 +4,10 @@ class Cards_Stack:
     def __init__(self):
         self.cards = [x for x in range(1, 11)]*4 + [10]*12
         random.shuffle(self.cards)
-        self.TOP = len(self.cards)-1
     def give(self):
         #Combination of Peek and Pop stack methods
-        card = self.cards[self.TOP]
+        card = self.cards[-1]
         self.cards.pop()
-        self.TOP -= 1
         return card
 
 cards = Cards_Stack()
