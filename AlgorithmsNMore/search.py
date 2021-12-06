fea94116dac71c681518dbbbca276b41b65ffa83
TomiG06 <thomasgkountas@gmail.com>
2021-12-06 21:13:31 +0200

    completed binary search

--- AlgorithmsNMore/search.py
+++ AlgorithmsNMore/search.py
@@ -2,16 +2,15 @@
 def binarySearch(l, n):
     low = 0
     high = len(l)-1
-    while True:
+    while low<=high:
         mid = (high+low)//2
         if l[mid] == n:
-            print(mid)
-            break
+            return mid
         elif l[mid] < n:
             low = mid+1
         else:
             high = mid-1
-
+    return -1
 
 #Linear Search
 def linearSearch(l, n):
