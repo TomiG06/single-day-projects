623a36c7d356a8084388d083481dba0b7917d6c7
TomiG06 <thomasgkountas@gmail.com>
2021-12-11 18:53:32 +0200

    added classes

--- blackJack.py
+++ blackJack.py
@@ -10,43 +10,47 @@ class Cards_Stack:
         self.cards.pop()
         return card
 
-cards = Cards_Stack()
-
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, player=True):
         self.cards = 0
         self.plays = True
+        self.player = player
         self.name = name
     
     def hit(self, card):
         if card == 1:
-            if self != opponent:
+            if self.player:
                 card = 1 if input("You got an Ace!\nWill you count it as 1 or 11?") == "1" else 11
             else:
                 card == 1 if self.cards+11 > 21 else 11
         self.cards += card
         if self.cards >= 21:
-            print(f"{player.name}: {player.cards}\nOpponent: {opponent.cards}")
+            print(f"{self.name}: {self.cards}")
             if self.cards == 21:
                 print(f"{self.name} won")
             else:
-                print(f"{player.name} got burnt\nBob won!") if self == player else print(f"Bob got burnt\n{player.name} won")
+                print(f"{self.name} got burnt")
             sys.exit()
 
     def stay(self):
          self.plays = False
 
-player = Player(input("Enter name: "))
-opponent = Player("Bob")
+def main():
+    cards = Cards_Stack()
+    player = Player(input("Enter name: "))
+    opponent = Player("Bob", False)
 
-while player.cards < 21 and opponent.cards < 21 and player.plays:
-    print(f"{player.name}: {player.cards}\nOpponent: {opponent.cards}")
-    if input("Hit or Stay(H, S)? ").upper() == "H":
-        player.hit(cards.give())
+    while player.cards < 21 and opponent.cards < 21 and player.plays:
+        print(f"{player.name}: {player.cards}\nOpponent: {opponent.cards}")
+        if input("Hit or Stay(H, S)? ").upper() == "H":
+            player.hit(cards.give())
 
-    else:
-        player.stay()
-    opponent.hit(cards.give())
-    if not player.plays:
-        while True:
-            opponent.hit(cards.give())
+        else:
+            player.stay()
+        opponent.hit(cards.give())
+        if not player.plays:
+            while True:
+                opponent.hit(cards.give())
+
+if __name__ == "__main__":
+    main()
--- tictactoe.py
+++ tictactoe.py
@@ -1,66 +1,73 @@
 from pandas import DataFrame
 import numpy as np
-import os
 
-table = np.array([[" ", " ", " "]] * 3)
+class TicTacToe:
+    def __init__(self):
+        self.table = np.array([[" ", " ", " "]] * 3)
+        self.valid = ["a0", "a1", "a2", "b0", "b1", "b2", "c0", "c1", "c2"]
+        self.marks = ["X", "O"]
 
-valid = ["a0", "a1", "a2", "b0", "b1", "b2", "c0", "c1", "c2"]
+    def deptab(self):
+        print(DataFrame(self.table, columns=["a", "b", "c"]))
 
-marks = ["X", "O"]
+    def deploy(self, position, m):
+        columns = {"a":0, "b":1, "c":2}
+        y, x = position
+        x = int(x)
+        if self.table[x][columns.get(y)] == " ":
+            self.table[x][columns.get(y)] = m
+            self.valid.remove(y+str(x))
+            return True
+        return False
 
-def deptab():
-    print(DataFrame(table, columns=["a", "b", "c"]))
+    def check(self):
+        for x in self.table:
+            if all([y == x[0] and y in ['X', 'O'] for y in x]):
+                    return "Computer Wins" if x[0] == "O" else "Player Wins"
 
-def deploy(x, y, m):
-    global table, valid
-    columns = {"a":0, "b":1, "c":2}
-    if table[x][columns.get(y)] == " ":
-        table[x][columns.get(y)] = m
-        valid.remove(y+str(x))
-        return True
-    return False
+        c1, c2, c3 = [self.table[x] for x in range(3)]
+        for l in range(3):
+            if c1[l] == c2[l] == c3[l] and all([self.table[x][l] in self.marks for x in range(3)]):
+                return "Computer Wins" if c1[l] == "O" else "Player Wins"
+        
+        if self.table[0][0] == self.table[1][1] == self.table[2][2] and all(self.table[x][x] in self.marks for x in range(3)):
+            return "Computer Wins" if self.table[1][1] == "O" else "Player Wins"
 
-def check():
-    for x in table:
-        if all([y == x[0] and y in ['X', 'O'] for y in x]):
-                return "Computer Wins" if x[0] == "O" else "Player Wins"
+        if self.table[2][0] == self.table[1][1] == self.table[0][2] and all(self.table[abs(x-2)][x] in self.marks for x in range(3)):
+            return "Computer Wins" if self.table[1][1] == "O" else "Player Wins"
+        
+        if all(" " not in self.table[x] for x in range(3)):
+            return "Draw"
 
-    c1, c2, c3 = [table[x] for x in range(3)]
-    for l in range(3):
-        if c1[l] == c2[l] == c3[l] and all([table[x][l] in marks for x in range(3)]):
-            return "Computer Wins" if c1[l] == "O" else "Player Wins"
-    
-    if table[0][0] == table[1][1] == table[2][2] and all(table[x][x] in marks for x in range(3)):
-        return "Computer Wins" if table[1][1] == "O" else "Player Wins"
+class Computer:
+    def move(self):
+        board.deploy(np.random.choice(board.valid), "O")
 
-    if table[2][0] == table[1][1] == table[0][2] and all(table[abs(x-2)][x] in marks for x in range(3)):
-        return "Computer Wins" if table[1][1] == "O" else "Player Wins"
-    
-    if all(" " not in table[x] for x in range(3)):
-        return "Draw"
+if __name__ == "__main__":
+    import os
 
-def com():
-    p = np.random.choice(valid)
-    deploy(int(p[1]), p[0], "O")
+    computer = Computer()
+    board = TicTacToe()
 
-while True:
-    os.system("clear")
-    print('Player: X\nComputer: O')
-    deptab()
-    print(valid)
-    print()
-    c = check()
-    if c != None:
-        print(c)
-        break
     while True:
-        p = input("Enter position(a0): ")
-        try:
-            ver = deploy(int(p[1]), p[0], 'X')
-            if ver == True:
-                com() if check() == None else print(check())
-                break
-            else:
-                print('That position is already assigned')
-        except (ValueError, IndexError, KeyError):
-            print("Enter a valid position")
+        os.system("clear")
+        print('Player: X\nComputer: O')
+        board.deptab()
+        print(board.valid)
+
+        c = board.check()
+        if c != None:
+            print(c)
+            break
+        
+        while True:
+            try:
+                ver = board.deploy(input("Enter position(a0): "), 'X')
+                if ver == True:
+                    if board.check() == None: computer.move() 
+                    else: print(board.check())
+                    break
+                else:
+                    print('That position is already assigned')
+            except (ValueError, IndexError, KeyError):
+                print("Enter a valid position")
