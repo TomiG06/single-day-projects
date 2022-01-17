e16224bdf4e3d266c3993a8621a05dd10abaad61
TomiG06 <thomasgkountas@gmail.com>
2022-01-17 15:01:19 +0200

    changed it in order to work with more rotations

--- AlgorithmsNMore/rot13.c
+++ AlgorithmsNMore/rot13.c
@@ -6,21 +6,21 @@ int ascii(char c) {
     return (int) "%d", c;
 }
 
-#define MIN ascii('a')
-#define MAX MIN + 25
+#define MIN ascii('a')-1
+#define MAX MIN + 26
 #define ROT 13
+//Works for ROT in {0,...,26}
 
 int main() {
     char txt[100];
     printf("Enter word in lowercase: ");
     scanf("%s", txt);
-    
     for(int x = 0; x < strlen(txt); ++x) {
         int ascval = ascii(txt[x]);
-        if(ascval <= MIN + 12) {
+        if(ascval <= MAX-ROT) {
             printf("%c", ascval + ROT);
         } else {
-            printf("%c", ascval - ROT);
+            printf("%c", ascval - 26 + ROT);
         }
     }
     printf("\n");
