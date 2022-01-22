3b48749e825f2577ebaea193667eb653c8a57ac4
TomiG06 <thomasgkountas@gmail.com>
2022-01-22 18:24:31 +0200

    added log/sign in system + deletion system

--- telephone_directory/helpers.hpp
+++ telephone_directory/helpers.hpp
@@ -5,7 +5,8 @@
 #include <fstream>
 #include <string>
 
-#define FILE_NAME "instances.txt"
+#define NUMS_FILE "instances.txt"
+#define PROFILE "profiles.txt"
 using namespace std;
 
 void add_number(string fname, string lname, string number) {
@@ -14,41 +15,114 @@ void add_number(string fname, string lname, string number) {
         return;
     }
     ofstream file;
-    file.open(FILE_NAME, ios::app);
+    file.open(NUMS_FILE, ios::app);
     file << lname << "," << fname << "," << number << endl;
     file.close();
     cout<<"Instance added successfully!\n";
 }
 
-bool inst_exists(string fn, string ln, string n) {
+bool exists(string inst, string file_name) {
     string line;
-    string inst = ln + "," + fn + "," + n + "\n";
-    ifstream file(FILE_NAME);
+    ifstream file(file_name);
     while(getline(file, line)) {
-        if(inst == line || n == line.substr(line.length() - 10)) {
-            return true;
+        if(file_name == NUMS_FILE) {
+            if(inst == line || inst.substr(inst.length()-11) == line.substr(line.length() - 10) + '\n') {
+                return true;
+            }
+        } else {
+            if(inst == line.substr(0, line.find(','))) { return true; }
         }
     }
     return false;
 }
 
 string search(string props, char status) {
-    string line, name;
-    ifstream file(FILE_NAME);
+    string line, name, list_of_names;
+    ifstream file(NUMS_FILE);
     if(status == '1') {
         int i = props.find(' ');
         name = props.substr(i+1) + "," + props.substr(0, i);
     }
     while(getline(file, line)) {
         if(status == '1') {
-            if(name == line.substr(0, line.length()-11)) { return line; }
+            if(name == line.substr(0, line.length()-11)) { list_of_names += "-" + line; }
             continue;
         }
         if(status == '2') {
             if(line.substr(line.length()-10, line.length()) == props) { return line; }
         }
     }
-    return "No profile\n";
+    if(status == '1') { return list_of_names; }
+    return "There is no name with that number\n";
+}
+
+bool sign_log(string name, string pass, bool sign=false) {
+    if(sign) {
+        if(exists(name, PROFILE)) {
+            cout << "Username " + name + " is already taken\n";
+            return false;
+        }
+        ofstream file;
+        file.open(PROFILE, ios::app);
+        file << name << "," << pass << endl;
+        file.close();
+        cout << "Signed in successfully!\n";
+        return true;
+    }
+    string line;
+    ifstream file(PROFILE);
+    while(getline(file, line)) {
+        if(name == line.substr(0, line.find(','))) {
+            if(pass == line.substr(line.find(',')+1, line.length()-1)) {
+                cout << "Logged in successfully!\n";
+                return true;
+            }
+            cout << "Wrong password\n";
+            return false;
+        }
+    }
+    cout << "There is no profile with that name\nBut you can use sign in to create one\n";
+    return false;
+}
+
+void rmv(string inst, string file_name) {
+    string line, content, msg;
+    ifstream file(file_name);
+    if(file_name == NUMS_FILE) {
+        inst = search(inst, '2');
+        if(!exists(inst, NUMS_FILE)) { 
+            cout << "Our database contains no name with phone number " + inst.substr(inst.length()-10, inst.length()-1) + '\n';
+            return;
+        } 
+        msg = "Number deleted successfully\n";
+    } else {
+        if(!exists(inst.substr(0, inst.find(',')), PROFILE)) {
+            cout << "There is no profile with the name " + inst.substr(0, inst.find(',')) + '\n';
+            return;
+        }
+        msg = "Account deleted successfully\n";
+    }
+    while(getline(file, line)) {
+        if(file_name == NUMS_FILE) {
+            if(inst != line) { content += line + '\n'; }
+        } else {    
+            if(inst.substr(0, inst.find(',')) == line.substr(0, line.find(','))) {
+                if(inst.substr(inst.find(',') + 1, inst.length()-1) != line.substr(line.find(',') + 1, line.length()-1)) {
+                    cout << "Wrong password\n";
+                    return;
+                }
+                cout << "Deleting Account..\n";
+            } else {
+                content += line + '\n';
+            }
+        }
+    }
+    file.close();
+
+    ofstream wfile(file_name);
+    wfile << content;
+    wfile.close();
+    cout << msg;
 }
 
 #endif
--- telephone_directory/teldir.cpp
+++ telephone_directory/teldir.cpp
@@ -2,10 +2,66 @@
 #include <string>
 #include "helpers.hpp"
 
+#define SUP "ih8cpp" //SUper Password for admins or something
+
 int main() {
-    cout<<"Telephone Directory\n";
-    cout << "1.Add number\n2.Search Number\n3.Exit\n";
-    string fn, ln, n, cmd, prop;
+    string cmd;
+    {
+        string name, pass;
+        bool log_stat = false;
+        cout<<"Telephone Directory\n";
+        cout<<"1.Sign in\n2.Log in\n3.Delete Account\n4.Exit\n";
+        while(1) {
+            cout << ">>";
+            cin>>cmd;
+            switch(cmd[0]) {
+                case '1': case '2':
+                    cout << "Enter name: ";
+                    cin >> name;
+                    cout << "Enter password: ";
+                    cin >> pass;
+                    if(cmd[0] == '1') {
+                        log_stat = sign_log(name, pass, true);   
+                    } else if(cmd[0] == '2') {
+                        log_stat = sign_log(name, pass);
+                    } else {
+                        cout << "Invalid Input\n";
+                    }
+                    break;
+                case '3':
+                    cout << "1.Name-SUP\n2.Name-Pass\n";
+                    cin >> cmd;
+                    cout << "Enter name: ";
+                    cin >> name;
+                    cout << "Enter password: ";
+                    cin >> pass;
+                    if(cmd == "1") {
+                        if(pass == SUP) {
+                            string line;
+                            ifstream sfile(PROFILE);
+                            while(getline(sfile, line)) {
+                                if(name == line.substr(0, line.find(','))) { break; }
+                            }
+                            rmv(line ,PROFILE);
+                        }
+                    } else if(cmd == "2") {
+                        rmv(name + ',' + pass, PROFILE);
+                    }
+                    break;
+                case '4':
+                    cout<<"Terminating..\n";
+                    return 0;
+                default:
+                    cout<<"Invalid Input\n";
+            }
+            if(log_stat) {
+                system("clear");
+                break;
+            }
+        }
+    }
+    cout << "1.Add number\n2.Search Number\n3.Delete number (SUP)\n4.Log out\n";
+    string fn, ln, n, prop;
     while(true) {
         cout << "Cmd: ";
         cin >> cmd;
@@ -17,14 +73,14 @@ int main() {
                 cin >> ln;
                 cout << "Enter phone number: ";
                 cin >> n;
-                if(inst_exists(fn, ln, n)) {
+                if(exists(ln + "," + fn + "," + n + "\n", NUMS_FILE)) {
                     cout << "Instance already exists\n";
                     break;
                 }
                 add_number(fn, ln, n);
                 break;
             case '2':
-                cout<<"1.Search by name(<first name> <last name>)\n2.Search by phone number\n";
+                cout<<"1.Search by name(<first name> <last name>)\n2.Search by phone number\n>> ";
                 cin >> cmd;
                 if(cmd[0] != '1' && cmd[0] != '2') {
                     cout<<"Invalid command\n";
@@ -36,6 +92,17 @@ int main() {
                 cout << search(prop, cmd[0]) << endl;
                 break;
             case '3':
+                cout << "Enter SUP: ";
+                cin >> cmd;
+                if(cmd == SUP) {
+                    cout << "Enter phone number: ";
+                    cin >> cmd;
+                    rmv(cmd, NUMS_FILE);
+                } else {
+                    cout << "Wrong SUP\n";
+                }
+                break;
+            case '4':
                 cout<<"Terminating...\n";
                 return 0;
             default:
