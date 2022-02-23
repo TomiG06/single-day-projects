277c7df96be09e427aef4358ef3fb9ce7ef165fe
TomiG06 <thomasgkountas@gmail.com>
2022-02-23 15:17:17 +0200

    input as cl arg and removed useless curly braces

--- AlgorithmsNMore/sieveOfEratosthenesbutinc.c
+++ AlgorithmsNMore/sieveOfEratosthenesbutinc.c
@@ -4,26 +4,19 @@
 //Sieve of Eratosthenes (But in C)
 //creates an array where non zero elements are prime numbers
 
-int main() {
-        int limit;
-        printf("Limit: ");
-        scanf("%d", &limit);
-        int len = limit-2;
-        int* range = (int*) malloc(len*sizeof(int));
-	for(int i; i<len; ++i) {
-		range[i] = i+2;
-	}
-        for(int x = 0; x<len; ++x) {
-                int num = range[x];
-                if(num*num > limit) { break; }
-                if(num == 0) { continue; }
-                for(int y=0; y<len; ++y) {
-                        if(range[y]%num == 0 && range[y] != num) { range[y] = 0; }
-                }
-        }
-        for(int x=0; x<len; ++x) {
-    		if(range[x]!=0) { printf("%d\n", range[x]);	}
-        }
-        free(range);
-        return 0;
+int main(int argc, char* argv[]){
+    if(argc == 1) exit(1);
+    int limit = atoi(argv[1]);
+    int len = limit-2;
+    int* range = (int*) malloc(len*sizeof(int));
+	for(int i; i<len; ++i) range[i] = i+2;
+    for(int x = 0; x<len; ++x) {
+        int num = range[x];
+        if(num*num > limit) break;
+        if(num == 0) continue;
+        for(int y=0; y<len; ++y) if(range[y]%num == 0 && range[y] != num) range[y] = 0;
+    }
+    for(int x=0; x<len; ++x) if(range[x]!=0) printf("%d\n", range[x]);
+    free(range);
+    return 0;
 }
